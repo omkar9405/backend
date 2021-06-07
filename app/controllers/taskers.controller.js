03ca50e0f2c63a9f59cdde0dfea9b705b93a6824
omkar9405 <omkarp1101@rediffmail.com>
2021-06-07 15:31:41 +0530

    resete

--- app/controllers/taskers.controller.js
+++ app/controllers/taskers.controller.js
@@ -7,15 +7,6 @@ var code;
 
 //login
 exports.login =async (req,res)=>{
-  // Validate request
-  // const errors = validationResult(req);
-  // if(!errors.isEmpty())
-  //   {
-  //     return res.status(400).json({
-  //       errors: errors.array()
-  //     });
-  //   }
- 
     const {username,password}=req.body;
  
     try{
@@ -299,9 +290,7 @@ try
       transporter.sendMail({
       to: email,
       subject: "noreply@atyourservice - Password Reset mail",
-      html: `
-      your password reset code is ${code}
-      `,
+      html: `your password reset code is ${code}`,
     }).then(data=>{
     res.status(200).send({
       message:"success" +code 
@@ -314,7 +303,6 @@ try
         error.message
     });
   }
-  
 };
 
   //patch Password
@@ -342,16 +330,16 @@ try
     console.log(req.body);
     if(req.params.code==newcode){
       console.log(req.body);
-    Tasker.update({id},{$set:req.body} ,{ useFindAndModify: false })
+    Tasker.updateOne({id},{$set:req.body})
       .then(data => {
         console.log(req.body);
        res.status(200).send({
-          message: "Password was updated successfully." + id + res.body
+          message: "Password was updated successfully." + id 
         })
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error updating Password status with id="+ id + res.body
+          message: "Error updating Password status with id="+ id
         })
       })
     // res.status(200).send({
